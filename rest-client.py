cea389b5656982d1908588aab4498879c336cd26
firodiaPurva <pufi1846@colorado.edu>
2024-10-19 19:08:03 -0600

    Initial

--- rest-client.py
+++ rest-client.py
@@ -4,33 +4,35 @@ import requests
 import sys
 from time import perf_counter
 
-# Arguments: machine, api route, number of runs
+# Parse command line arguments to indicate the endpoint and iterations to test
 addr = sys.argv[1]
 endpoint = sys.argv[2]
 num_tests = int(sys.argv[3])
 print(addr, endpoint, num_tests)
 
-# Prepare headers
+# Prepare headers for different HTTP requests
 headers = {'content-type': 'image/png'}
 headers_add = {'content-type': 'text/plain'}
 
+# Load the image for the rawimg and jsonimg tests
 img = open('Flatirons_Winter_Sunrise_edit_2.jpg', 'rb').read()
 
-# URLs for the different endpoints
-add_url = addr + '/api/add/5/2'
-image_url = addr + '/api/image'
-rawimg_url = addr + '/api/rawimg'
-jsonimg_url = addr + '/api/jsonimg'
-dotproduct_url = addr + '/api/dotproduct'
+# Correct the URLs to include 'http://'
+add_url = 'http://' + addr + '/api/add/5/2'
+image_url = 'http://' + addr + '/api/image'
+rawimg_url = 'http://' + addr + '/api/rawimg'
+jsonimg_url = 'http://' + addr + '/api/jsonimg'
+dotproduct_url = 'http://' + addr + '/api/dotproduct'
 
-# Measure performance for different endpoints
+# Perform the tests based on the selected endpoint
 if endpoint == 'add':
     start_t1 = perf_counter()
     for i in range(num_tests):
         resp = requests.get(add_url, headers=headers_add)
         print("Response is", resp.text)
     stop_t1 = perf_counter()
-    avg = (stop_t1 - start_t1) / num_tests
+    total = stop_t1 - start_t1
+    avg = total / num_tests
     print("The average time taken for the /api/add endpoint was {}".format(avg))
 
 elif endpoint == 'image':
@@ -39,32 +41,42 @@ elif endpoint == 'image':
         resp = requests.post(image_url, data=img, headers=headers)
         print("Response is", resp)
     stop_t2 = perf_counter()
-    avg = (stop_t2 - start_t2) / num_tests
+    total = stop_t2 - start_t2
+    avg = total / num_tests
     print("The average time taken for the /api/image endpoint was {}".format(avg))
 
 elif endpoint == 'rawimg':
+    # Use POST for rawimg
     start_t3 = perf_counter()
     for i in range(num_tests):
         resp = requests.post(rawimg_url, data=img, headers=headers)
-        print("Response is", resp.text)
+        print("Response is", resp)
     stop_t3 = perf_counter()
-    avg = (stop_t3 - start_t3) / num_tests
+    total = stop_t3 - start_t3
+    avg = total / num_tests
     print("The average time taken for the /api/rawimg endpoint was {}".format(avg))
 
 elif endpoint == 'jsonimg':
+    # Use POST for jsonimg
     start_t4 = perf_counter()
     for i in range(num_tests):
         resp = requests.post(jsonimg_url, data=img, headers=headers)
-        print("Response is", resp.text)
+        print("Response is", resp)
     stop_t4 = perf_counter()
-    avg = (stop_t4 - start_t4) / num_tests
+    total = stop_t4 - start_t4
+    avg = total / num_tests
     print("The average time taken for the /api/jsonimg endpoint was {}".format(avg))
 
 elif endpoint == 'dotproduct':
+    # Use POST for dotproduct
     start_t5 = perf_counter()
     for i in range(num_tests):
         resp = requests.post(dotproduct_url, headers=headers_add)
         print("Response is", resp.text)
     stop_t5 = perf_counter()
-    avg = (stop_t5 - start_t5) / num_tests
+    total = stop_t5 - start_t5
+    avg = total / num_tests
     print("The average time taken for the /api/dotproduct endpoint was {}".format(avg))
+
+else:
+    print("Invalid endpoint. Use 'add', 'image', 'rawimg', 'jsonimg', or 'dotproduct'.")
