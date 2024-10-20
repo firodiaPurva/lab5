04ee0949bfa70062ba7ff570867193e401cb7304
firodiaPurva <pufi1846@colorado.edu>
2024-10-19 18:47:07 -0600

    Initial

--- rest-client.py
+++ rest-client.py
@@ -8,63 +8,45 @@ from time import perf_counter
 # We parse command line arguments to indicate the endpoint and iterations to test as follows: machine, api route, number of runs
 # python3 rest-client.py localhost add 1000
 
-# The address the that of the server we are sending a request to
+# The address of the server we are sending a request to
 addr = sys.argv[1]
 endpoint = sys.argv[2]
 num_tests = int(sys.argv[3])
 print(addr, endpoint, num_tests)
 
-# Address of our server
-# addr = 'http://localhost:5000'
-
-# In the following, we are loading in our image and sending it to the /api/image endpoint via a request, and getting back the image size
-# Next, we are making a request to the /api/add endpoint and getting back the sum in the response from the server
-
-# prepare headers for different http requests
+# Prepare headers for different HTTP requests
 headers = {'content-type': 'image/png'}
 headers_add = {'content-type': 'text/plain'}
 
 img = open('Flatirons_Winter_Sunrise_edit_2.jpg', 'rb').read()
-# send http request with image and receive response the appropriate amount of times specified
 image_url = addr + '/api/image'
-# url to send an http request to, in order to receive the sume of these hard-coded values
 add_url = addr + '/api/add/5/2'
 
 if endpoint == 'add':
-
     # Start timer before loop and stop after, then divide by the number of test requests
     start_t1 = perf_counter()
     for i in range(num_tests):
         resp = requests.get(add_url, headers=headers_add)
-        # decode response
+        # Decode response
         print("Response is", resp.text)
-        # print(json.loads(resp.text))
     stop_t1 = perf_counter()
 
     total = stop_t1 - start_t1
     avg = total / num_tests
     print("The average time taken for the /api/add endpoint was {}".format(avg))
 
-else:
-
-    # Start timer before loop and stop after, then divide by the number of tests for the image endpoint
-    # Here we remember to include the image data in the POST request, along with the url and the headers
+elif endpoint == 'image':
+    # Start timer before loop for the image endpoint
     start_t2 = perf_counter()
     for i in range(num_tests):
         resp = requests.post(image_url, data=img, headers=headers)
-        # decode response
+        # Decode response
         print("Response is", resp)
-        # commenting out the next line since for the image, the response is different
-        # print(json.loads(resp.text))
     stop_t2 = perf_counter()
 
     total = stop_t2 - start_t2
     avg = total / num_tests
-    print("The average time taken from the /api/image endpoint was {}".format(avg))
+    print("The average time taken for the /api/image endpoint was {}".format(avg))
 
-
-
-# response = requests.post(image_url, data=img, headers=headers)
-# decode response
-# print("Response is", response)
-# print(json.loads(response.text))
+else:
+    print("Invalid endpoint specified. Please use 'add' or 'image'.")
--- rest-server.py
+++ rest-server.py
@@ -1,71 +1,54 @@
 #!/usr/bin/env python3
-##
-## Sample Flask REST server implementing two methods
-##
-## Endpoint /api/image is a POST method taking a body containing an image
-## It returns a JSON document providing the 'width' and 'height' of the
-## image that was provided. The Python Image Library (pillow) is used to
-## proce#ss the image
-##
-## Endpoint /api/add/X/Y is a post or get method returns a JSON body
-## containing the sum of 'X' and 'Y'. The body of the request is ignored
-##
-##
 from flask import Flask, request, Response
 import jsonpickle
 import numpy as np
 from PIL import Image
 import io
+from time import perf_counter
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-
-# route http posts to this method(ie: client sends us a picture) AKA: we're getting the image data but then still returning something(the dimensions of the image)
 @app.route('/api/image', methods=['POST'])
 def test():
     r = request
-    # convert the data to a PIL image type so we can extract dimensions
+    start_time = perf_counter()  # Start timing
     try:
         ioBuffer = io.BytesIO(r.data)
         img = Image.open(ioBuffer)
-    # build a response dict to send back to client
         response = {
-            'width' : img.size[0],
-            'height' : img.size[1]
-            }
-    except:
-        response = { 'width' : 0, 'height' : 0}
-    # encode response using jsonpickle
+            'width': img.size[0],
+            'height': img.size[1]
+        }
+    except Exception as e:
+        response = {'width': 0, 'height': 0}
+    
+    # Measure JSON encoding time
+    encoding_start_time = perf_counter()
     response_pickled = jsonpickle.encode(response)
+    encoding_time = perf_counter() - encoding_start_time
+    print(f"JSON encoding time: {encoding_time:.6f} seconds")  # Log the time
+
+    processing_time = perf_counter() - start_time  # Calculate processing time
+    print(f"Image processing time: {processing_time:.6f} seconds")  # Log the time
 
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
-
-# route http gets to this method(ie: client wants the sum of the two numbers
 @app.route('/api/add/<int:x>/<int:y>', methods=['GET'])
-def test_add(x,y):
-    # Alternatively we could have done a POST method where the client(user) sends us numbers, not just hitting a hardcoded route
-    # req = request
-    # Try to add the numbers and return the sum or else return 0, note that the endpoint gets us the values as ints so no need to convert
-    # Also for this method, the body of the request is ignored since we're getting the numbers from the endpoint
+def test_add(x, y):
     try:
         total = x + y
-        # We put our response in a dictionary as key value pairs after doing the processing above. with jsonpickle.encode(response) the respons and return Response(response, status, mimetype)
         resp = {
             'sum': total
         }
-
     except:
         resp = {
             'sum': 0
         }
 
-    # Now we pickle the response and return it to the client as true json
+    # Now we pickle the response and return it to the client as true JSON
     pickled = jsonpickle.encode(resp)
-
     return Response(response=pickled, status=200, mimetype="application/json")
 
-
-# start flask app, using 0.0.0.0 allows us to hit the flask app via the ip address of the machine is on rather than just localhost
+# Start the Flask app
 app.run(host="0.0.0.0", port=5000)
